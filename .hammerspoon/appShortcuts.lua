ae4dfddcf622c64db4470f2f971f6936d9c01054
Jonathan <mrhandsomepixel@gmail.com>
2018-12-21 20:56:21 -0500

    Modularize the creation of app-specific keybinds/shortcuts

--- .hammerspoon/appShortcuts.lua
+++ .hammerspoon/appShortcuts.lua
@@ -1,52 +1,23 @@
-preview_shortcuts = hs.hotkey.modal.new()
-
-preview_shortcuts:bind({'cmd', 'alt'}, 's', function()
-  hs.eventtap.keyStroke('cmd', 'k')
-  hs.eventtap.keyStroke('cmd', 's')
-  hs.eventtap.keyStroke('cmd', 'w')
-end)
-
-function previewWatcher(appName, eventType, appObject)
-  if (appName == 'Preview') then
-    if (eventType == hs.application.watcher.activated) then
-      preview_shortcuts:enter()
-      -- Preview is in focus, so enable our preview_shortcuts
-    elseif (eventType == hs.application.watcher.deactivated) then
-      preview_shortcuts:exit()
-      -- Preview is out of focus, so enable our preview_shortcuts
+shortcut_template = function(app_name, mod, key, action)
+  local shortcut = hs.hotkey.new(mod, key, function()
+    local hsek = hs.eventtap.keyStroke() -- Shorthand for those that want to do keystrokes without typing the syntax entirely
+    action()
+  end)
+  local watcher = hs.application.watcher.new(function(name, eventType, app)
+    if eventType ~= hs.application.watcher.activated then return end
+    if name == app_name then
+      shortcut:enable() -- Enable the shortcut if the current activated app is the app_name you specified
+    else
+      shortcut:disable() -- Disable otherwise
     end
-  end
+  end)
+  watcher:start()
 end
 
--- Create and start the application event watcher
-watcher = hs.application.watcher.new(previewWatcher)
-watcher:start()
+preview_crop_save_close = shortcut_template
 
--- Activate the modal state
-preview_shortcuts:enter()
-review_shortcuts = hs.hotkey.modal.new()
-
-preview_shortcuts:bind({'cmd', 'alt'}, 's', function()
-  hs.eventtap.keyStroke('cmd', 'k')
-  hs.eventtap.keyStroke('cmd', 's')
-  hs.eventtap.keyStroke('cmd', 'w')
+preview_crop_save_close('Preview', {'cmd', 'option'}, 's', function()
+  hsek({'cmd'}, 'k')
+  hsek({'cmd'}, 's')
+  hsek({'cmd'}, 'w')
 end)
-
-function previewWatcher(appName, eventType, appObject)
-  if (appName == 'Preview') then
-    if (eventType == hs.application.watcher.activated) then
-      preview_shortcuts:enter()
-      -- Preview is in focus, so enable our preview_shortcuts
-    elseif (eventType == hs.application.watcher.deactivated) then
-      preview_shortcuts:exit()
-      -- Preview is out of focus, so enable our preview_shortcuts
-    end
-  end
-end
-
--- Create and start the application event watcher
-watcher = hs.application.watcher.new(previewWatcher)
-watcher:start()
-
--- Activate the modal state
-preview_shortcuts:enter()
