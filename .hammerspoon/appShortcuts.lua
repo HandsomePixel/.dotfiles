986ae80cf3169e9419759585e46bde06b82e6c59
Jonathan <mrhandsomepixel@gmail.com>
2018-12-27 12:12:57 -0500

    Improve modularity of creating app-specific shortcuts

--- .hammerspoon/appShortcuts.lua
+++ .hammerspoon/appShortcuts.lua
@@ -1,23 +1,37 @@
-shortcut_template = function(app_name, mod, key, action)
-  local shortcut = hs.hotkey.new(mod, key, function()
-    local hsek = hs.eventtap.keyStroke() -- Shorthand for those that want to do keystrokes without typing the syntax entirely
+function shortcut_template(__self, mod, key, action)
+  local keybind = hs.hotkey.new(mod, key, function()
     action()
   end)
   local watcher = hs.application.watcher.new(function(name, eventType, app)
     if eventType ~= hs.application.watcher.activated then return end
-    if name == app_name then
-      shortcut:enable() -- Enable the shortcut if the current activated app is the app_name you specified
+    if name == __self.appName then
+      keybind:enable() -- Enable the shortcut if the current activated app is the app_name you specified
     else
-      shortcut:disable() -- Disable otherwise
+      keybind:disable() -- Disable otherwise
     end
   end)
   watcher:start()
 end
 
-preview_crop_save_close = shortcut_template
+-- Preview custom shortcuts
+Preview = {
+  appName = 'Preview',
+  cropSaveClose = shortcut_template,
+}
 
-preview_crop_save_close('Preview', {'cmd', 'option'}, 's', function()
-  hsek({'cmd'}, 'k')
-  hsek({'cmd'}, 's')
-  hsek({'cmd'}, 'w')
+Preview:cropSaveClose({'cmd', 'option'}, 's', function()
+  hs.eventtap.keyStroke({'cmd'}, 'k')
+  hs.eventtap.keyStroke({'cmd'}, 's')
+  hs.eventtap.keyStroke({'cmd'}, 'w')
+end)
+
+-- Finder custom shortcuts
+Finder = {
+  appName = 'Finder',
+  openVolumes = shortcut_template,
+}
+
+Finder:openVolumes({'shift', 'option'}, 'v', function()
+  hs.execute('open /Volumes')
+  hs.alert('Volumes Folder Opened')
 end)
